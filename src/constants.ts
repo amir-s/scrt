90931b4438f8d5d4614fbdb2bec98051efce2233
Amir Saboury <amir.saboury@gmail.com>
2023-07-01 17:39:56 -0400

    0.0.11

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,5 @@
 export const ALGORITHM = 'aes-256-ctr';
+export const KEY_PAIR_TYPE = 'rsa';
 export const KEY_TYPE = 'pkcs1';
 export const KEY_FORMAT = 'pem';
 export const PRIVATE_KEY_CIPHER = 'aes-256-cbc';
